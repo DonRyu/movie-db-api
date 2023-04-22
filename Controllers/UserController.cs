ec19a99a642d062f066366d514d090946a616b2c
DonRyu <alpharyu1031@gmail.com>
2023-04-22 14:39:29 -0700

    add controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,8 +8,38 @@ namespace movie_db_api.Controllers
 	public class UserController : ControllerBase
     {
 		
-		
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			return Ok();
+		}
 
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(int id)
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		[Route("post")]
+		public async Task<IActionResult> Post([FromBody] User user)
+		{
+			return Ok();
+		}
+
+		[HttpPut]
+		[Route("put")]
+		public async Task<IActionResult> Put([FromBody] User user)
+		{
+			return Ok();
+		}
+
+		[HttpDelete]
+		[Route("delete")]
+		public async Task<IActionResult> Delete([FromBody] User user)
+		{
+			return Ok();
+		}
 	}
 }
 
