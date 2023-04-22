a0496597acee09b6e23ead2d5705891ba842ff84
DonRyu <alpharyu1031@gmail.com>
2023-04-21 17:25:34 -0700

    inital

---
+++ Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace movie_db_api.Controllers
+{
+	[ApiController]
+	[Route("controller")]
+	public class UserController : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+		public UserController()
+		{
+
+		}
+	}
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace movie_db_api.Models
+{
+	public class User
+	{
+        public int id { get; set; }
+    }
+}
+
