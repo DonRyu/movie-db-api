3c41ebdfe8bbad44489b98b8cce15c3912914b89
DonRyu <alpharyu1031@gmail.com>
2023-04-22 18:07:44 -0700

    migration

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,43 +3,58 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace movie_db_api.Controllers
 {
-	[ApiController]
-	[Route("controller")]
-	public class UserController : ControllerBase
+    [ApiController]
+    [Route("controller")]
+    public class UserController : ControllerBase
     {
-		
-		[HttpGet]
-		public async Task<IActionResult> Get()
-		{
-			return Ok();
-		}
 
-		[HttpGet("{id}")]
-		public async Task<IActionResult> Get(int id)
-		{
-			return Ok();
-		}
+        private readonly ApiDbContext _context;
 
-		[HttpPost]
-		[Route("post")]
-		public async Task<IActionResult> Post([FromBody] User user)
-		{
-			return Ok();
-		}
+        public UserController(ApiDbContext context)
+        {
+            _context = context;
+        }
 
-		[HttpPut]
-		[Route("put")]
-		public async Task<IActionResult> Put([FromBody] User user)
-		{
-			return Ok();
-		}
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
 
-		[HttpDelete]
-		[Route("delete")]
-		public async Task<IActionResult> Delete([FromBody] User user)
-		{
-			return Ok();
-		}
-	}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            await _context.Users.FindAsync(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("post")]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            var newUser = new User { nickname = user.nickname, email = user.email, password = user.password };
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("put")]
+        public async Task<IActionResult> Put([FromBody] User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<IActionResult> Delete([FromBody] User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
 }
 
